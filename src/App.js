7274203bd3aeb1ed74c1f18a5cc09f33f859bcd0
JamesBerlioz <mavlut18@gmail.com>
2023-08-15 14:34:43 +0300

    learned PropTypes and Flow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from "react";
+//@flow
 
-export default function App() {
-  return <></>;
+import React from "react";
+import PropTypes from "prop-types";
+
+type Props = {
+  item: string,
+  cost?: number
+};
+
+export default function App(props: Props) {
+  return (
+    <div>
+      <h1>{props.item}</h1>
+      <p>Cost: {props.cost}</p>
+    </div>
+  );
 }
